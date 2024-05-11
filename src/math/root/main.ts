574b72684d30600f4cd7b08599998ae8ee97a178
FlareShip <lalitsingh.micro@gmail.com>
2024-05-11 16:58:20 +0530

    ✨ feat(math): root support
    Added support for taking square root, cube root and any root of numbers.

---
+++ src/math/root/main.ts
@@ -0,0 +1,18 @@
+/*-----------------------------------------------------------------------------------------------
+│                                       Root                                                    │
+-----------------------------------------------------------------------------------------------*/
+
+import type { int } from "../utils";
+
+
+export function square_root(x: int): int {
+  return x ** (1 / 2);
+}
+
+export function cube_root(x: int): int {
+  return x ** (1 / 3);
+}
+
+export function root(x: int, n: int): int {
+  return x ** (1 / n);
+}

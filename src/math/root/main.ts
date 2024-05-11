51d01fc19a73479771c1cd6e6d5b60aae70f8677
FlareShip <lalitsingh.micro@gmail.com>
2024-05-11 17:12:37 +0530

    💎 style(math): formated root and exponentiation files

--- src/math/root/main.ts
+++ src/math/root/main.ts
@@ -4,15 +4,14 @@
 
 import type { int } from "../utils";
 
-
 export function square_root(x: int): int {
-  return x ** (1 / 2);
+	return x ** (1 / 2);
 }
 
 export function cube_root(x: int): int {
-  return x ** (1 / 3);
+	return x ** (1 / 3);
 }
 
 export function root(x: int, n: int): int {
-  return x ** (1 / n);
+	return x ** (1 / n);
 }
